39683a153efd5e41fc85346db464e44924c289a8
Petros Tsavdaridis <peteropon@gmail.com>
2019-04-02 12:40:10 +0200

    Added recursive loop. Works in the beginning but then it multiplies the questions

--- script.js
+++ script.js
@@ -1,110 +1,108 @@
-const url = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+const url = "https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple";
 var data;
-var section = document.querySelector('section');
+var header = document.querySelector('header');
+var counter = 0;
+let h2 = document.createElement('h2');
 
-document.getElementById('startbutton').addEventListener('click', getData);
+var points = 0;
 
+document.getElementById('startbutton').onclick = getData;
 
+function play(){
 
-function assignAnswersToButtons(answers) {
-  let answerArea = document.getElementById('answers');
-  answers.forEach( (answer) => {
-    let answerButton = document.createElement('button');
-    answerButton.innerHTML = `<button class="answerButton">'${answer}'</button>`;
-    answerArea.appendChild(answerButton);
-    answerButton.addEventListener('click', function (e) {
-      if(answerButton.innerText == answer){
-        alert("we have response")
-      }
-    })
-  })
+    document.getElementById('startbutton').innerText = 'Restart';
+    getData();
+
 }
 
-function createAnswerButtons(result) {
-  var answers = [];
-  answers.push(result.correct_answer);
-  result.incorrect_answers.forEach((answer)=>{
-    answers.push(answer);
-  });
-
-  answers.sort(function (a, b) {
-    return 0.5 - Math.random();
-  });
-  console.log(answers);
-  return answers;
-}
-
-function renderData(array) {
-
-    let answerGiven;
-    do {
-        let counter = 0;
-
-        answerGiven = false;
-        let quizQuestion = document.getElementById('h2');
-        let question = document.createElement('h2');
-        quizQuestion.innerText = "";
-      question.innerText = array[counter].question;
-        console.log(array[counter].question);
-      section.appendChild(question);
-      var answers = [];
-      answers.push(array[counter].correct_answer);
-      console.log(array[counter].correct_answer);
-      array[0].incorrect_answers.forEach((answer) => {
+function renderData(quizQuestion) {
+    let nextButton = document.createElement('button');
+    h2.innerText = quizQuestion[0].question;
+    header.appendChild(h2);
+    var answers = [];
+    answers.push(quizQuestion[0].correct_answer);
+    console.log(quizQuestion[0].correct_answer);
+    quizQuestion[0].incorrect_answers.forEach((answer) => {
           answers.push(answer);
       });
 
-      answers.sort(function (a, b) {
+    answers.sort(function (a, b) {
           return 0.5 - Math.random();
       });
-      let answerArea = document.getElementById('answers');
-      answers.forEach((answer) => {
-          var answerButton = document.createElement('button');
-          answerButton.innerHTML = `<button class="answerButton">'${answer}'</button>`;
-          answerArea.appendChild(answerButton);
+    let answerArea = document.getElementById('answers');
+    var answerButton1 = document.getElementById('answer1');
+    answerButton1.innerText = answers[0];
+    answerArea.appendChild(answerButton1);
+    var answerButton2 = document.getElementById('answer2');
+    answerButton2.innerText = answers[1];
+    answerArea.appendChild(answerButton2);
+    var answerButton3 = document.getElementById('answer3');
+    answerButton3.innerText = answers[2];
+    answerArea.appendChild(answerButton3);
+    var answerButton4 = document.getElementById('answer4');
+    answerButton4.innerText = answers[3];
+    answerArea.appendChild(answerButton4);
 
-      });
-      answerArea.addEventListener('click', function (e) {
+    answerArea.addEventListener('click', function (e) {
           if (e.target.nodeName == 'BUTTON') {
-              if (e.target.innerText == ("'" + array[counter].correct_answer + "'")) {
+              //alert("something");
+              //console.log(e.target.innerText);
+              //console.log(quizQuestion[0].correct_answer);
+
+              if (e.target.innerText == (quizQuestion[0].correct_answer)) {
                   alert("correct!");
-                  console.log(array[counter].correct_answer);
+                  console.log(quizQuestion[0].correct_answer);
+                  points++;
+                  //h2.innerText = "";
+                  getData();
               } else {
                   alert("wrong answer");
-                  console.log(e.target.innerText);
-                  console.log(array[counter].correct_answer);
+                  //h2.innerText = "";
+                  getData();
               }
-              answerGiven == true;
-              counter++;
-              console.log(counter);
           }
       })
+      //             answerArea.innerText = "";
+      //             h2.innerText = "";
+      //             nextButton.innerHTML = `<button class="nextButton">Next question</button>`;
+      //             nextButton.addEventListener('click', getData);
+      //             section.appendChild(nextButton);
+      //             //if (counter < 5) getData();
+      //         } else {
+      //             alert("wrong answer");
+      //             console.log(e.target.innerText);
+      //             console.log(quizQuestion[0].correct_answer);
+      //             counter++;
+      //             answerArea.innerText = "";
+      //             h2.innerText = "";
+      //             nextButton.innerHTML = `<button class="nextButton">Next question</button>`;
+      //             nextButton.addEventListener('click', getData);
+      //             section.appendChild(nextButton);
+      //             //if (counter < 5) getData();
+      //         }
+      //
+      //     }
+      //
+      // });
 
-        if (counter == array.length) break;
-  }while (answerGiven)
+    //header.removeChild(h2);
 
-    //createAnswerButtons(array[0]);
-  //});
-
-  // data.forEach((result)=> {
-  //   quizQuestion.innerText = result.question;
-  //   section.appendChild(quizQuestion);
-  //   createAnswerButtons(result);
-  // })
 }
 
+
 function getData() {
-  const xhr = new XMLHttpRequest;
+    h2.innerText = "";
+
+    const xhr = new XMLHttpRequest;
   xhr.onreadystatechange = function() {
     if(xhr.readyState == 4){
       if(xhr.status == 200){
         console.log("Status: 200");
         data = xhr.response;
         renderData(data.results);
-        //createAnswerButtons(data.results);
       }
     }
-  }
+  };
   xhr.open('GET', url, true);
   xhr.responseType = 'json';
   xhr.send();
