66a309b0768a6314412d051bc26a2ff70739ec3f
Petros Tsavdaridis <peteropon@gmail.com>
2019-04-02 14:34:31 +0200

    Moved the recursion to an event listener for clicks, looping works

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-const url = "https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple";
+const url = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 var data;
+var quiz = [];
 var header = document.querySelector('header');
 var counter = 0;
 let h2 = document.createElement('h2');
+let answerArea = document.getElementById('answers');
+var iterator = 0;
 
 var points = 0;
 
-document.getElementById('startbutton').onclick = getData;
+document.getElementById('startbutton').onclick = play;
 
 function play(){
-
     document.getElementById('startbutton').innerText = 'Restart';
     getData();
+    console.log(quiz.length);
 
 }
 
-function renderData(quizQuestion) {
-    let nextButton = document.createElement('button');
-    h2.innerText = quizQuestion[0].question;
-    header.appendChild(h2);
-    var answers = [];
-    answers.push(quizQuestion[0].correct_answer);
-    console.log(quizQuestion[0].correct_answer);
-    quizQuestion[0].incorrect_answers.forEach((answer) => {
-          answers.push(answer);
-      });
+function getData() {
+    h2.innerText = "";
 
-    answers.sort(function (a, b) {
-          return 0.5 - Math.random();
-      });
-    let answerArea = document.getElementById('answers');
+    const xhr = new XMLHttpRequest;
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4){
+            if(xhr.status == 200){
+                console.log("Status: 200");
+                data = xhr.response;
+                quiz = data.results;
+                console.log(quiz.length);
+
+                renderData(quiz[iterator]);
+            }
+        }
+    };
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.send();
+}
+
+
+
+function assignButtons(answers) {
+//    answers.forEach( (answer)=> {
+//    });
     var answerButton1 = document.getElementById('answer1');
     answerButton1.innerText = answers[0];
     answerArea.appendChild(answerButton1);
@@ -42,26 +56,24 @@ function renderData(quizQuestion) {
     var answerButton4 = document.getElementById('answer4');
     answerButton4.innerText = answers[3];
     answerArea.appendChild(answerButton4);
+}
 
-    answerArea.addEventListener('click', function (e) {
-          if (e.target.nodeName == 'BUTTON') {
-              //alert("something");
-              //console.log(e.target.innerText);
-              //console.log(quizQuestion[0].correct_answer);
+function renderData(quizQuestion) {
+    //let nextButton = document.createElement('button');
+    h2.innerText = quizQuestion.question;
+    header.appendChild(h2);
+    var answers = [];
+    answers.push(quizQuestion.correct_answer);
+    console.log(quizQuestion.correct_answer);
+    quizQuestion.incorrect_answers.forEach((answer) => {
+          answers.push(answer);
+      });
 
-              if (e.target.innerText == (quizQuestion[0].correct_answer)) {
-                  alert("correct!");
-                  console.log(quizQuestion[0].correct_answer);
-                  points++;
-                  //h2.innerText = "";
-                  getData();
-              } else {
-                  alert("wrong answer");
-                  //h2.innerText = "";
-                  getData();
-              }
-          }
-      })
+    answers.sort(function (a, b) {
+          return 0.5 - Math.random();
+      });
+    assignButtons(answers);
+
       //             answerArea.innerText = "";
       //             h2.innerText = "";
       //             nextButton.innerHTML = `<button class="nextButton">Next question</button>`;
@@ -85,25 +97,17 @@ function renderData(quizQuestion) {
       //
       // });
 
-    //header.removeChild(h2);
-
 }
 
 
-function getData() {
-    h2.innerText = "";
 
-    const xhr = new XMLHttpRequest;
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4){
-      if(xhr.status == 200){
-        console.log("Status: 200");
-        data = xhr.response;
-        renderData(data.results);
-      }
-    }
-  };
-  xhr.open('GET', url, true);
-  xhr.responseType = 'json';
-  xhr.send();
-}
+    answerArea.addEventListener('click', function (e) {
+        if (e.target.nodeName == 'BUTTON') {
+            alert("something");
+            iterator++;
+            renderData(quiz[iterator]);
+        }
+    });
+
+
+
