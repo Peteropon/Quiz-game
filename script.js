ee3af22f5112df15ca05d06de4cd2ef2c74fd456
Petros Tsavdaridis <peteropon@gmail.com>
2019-04-03 14:16:15 +0200

    Split answers array into correct and wrong answers, restructured, added css classes for right and wrong answer

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const url = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multip
 var data;
 var quiz = [];
 var header = document.querySelector('header');
-var counter = 0;
+var points = 0;
 let h2 = document.createElement('h2');
 let answerArea = document.getElementById('answers');
 var iterator = 0;
+var wrongAnswers = [];
+var correctAnswer = "";
+var answerButton1 = document.getElementById('answer1');
+var answerButton2 = document.getElementById('answer2');
+var answerButton3 = document.getElementById('answer3');
+var answerButton4 = document.getElementById('answer4');
 
-var points = 0;
 
 document.getElementById('startbutton').onclick = play;
 
@@ -41,61 +46,34 @@ function getData() {
 
 
 
-function assignButtons(answers) {
+function assignButtons(wrongAnswers, correct) {
 //    answers.forEach( (answer)=> {
 //    });
-    var answerButton1 = document.getElementById('answer1');
-    answerButton1.innerText = answers[0];
+    answerButton1.innerText = correct;
     answerArea.appendChild(answerButton1);
-    var answerButton2 = document.getElementById('answer2');
-    answerButton2.innerText = answers[1];
+    answerButton2.innerText = wrongAnswers[0];
     answerArea.appendChild(answerButton2);
-    var answerButton3 = document.getElementById('answer3');
-    answerButton3.innerText = answers[2];
+    answerButton3.innerText = wrongAnswers[1];
     answerArea.appendChild(answerButton3);
-    var answerButton4 = document.getElementById('answer4');
-    answerButton4.innerText = answers[3];
+    answerButton4.innerText = wrongAnswers[2];
+
     answerArea.appendChild(answerButton4);
 }
 
 function renderData(quizQuestion) {
-    //let nextButton = document.createElement('button');
     h2.innerText = quizQuestion.question;
     header.appendChild(h2);
-    var answers = [];
-    answers.push(quizQuestion.correct_answer);
+    correctAnswer = quizQuestion.correct_answer;
+    //wrongAnswers.push(quizQuestion.correct_answer);
     console.log(quizQuestion.correct_answer);
     quizQuestion.incorrect_answers.forEach((answer) => {
-          answers.push(answer);
+          wrongAnswers.push(answer);
       });
 
-    answers.sort(function (a, b) {
+    wrongAnswers.sort(function (a, b) {
           return 0.5 - Math.random();
       });
-    assignButtons(answers);
-
-      //             answerArea.innerText = "";
-      //             h2.innerText = "";
-      //             nextButton.innerHTML = `<button class="nextButton">Next question</button>`;
-      //             nextButton.addEventListener('click', getData);
-      //             section.appendChild(nextButton);
-      //             //if (counter < 5) getData();
-      //         } else {
-      //             alert("wrong answer");
-      //             console.log(e.target.innerText);
-      //             console.log(quizQuestion[0].correct_answer);
-      //             counter++;
-      //             answerArea.innerText = "";
-      //             h2.innerText = "";
-      //             nextButton.innerHTML = `<button class="nextButton">Next question</button>`;
-      //             nextButton.addEventListener('click', getData);
-      //             section.appendChild(nextButton);
-      //             //if (counter < 5) getData();
-      //         }
-      //
-      //     }
-      //
-      // });
+    assignButtons(wrongAnswers, correctAnswer);
 
 }
 
@@ -103,11 +81,56 @@ function renderData(quizQuestion) {
 
     answerArea.addEventListener('click', function (e) {
         if (e.target.nodeName == 'BUTTON') {
-            alert("something");
+            //alert("something");
+            console.log(wrongAnswers);
+            wrongAnswers = [];
+
+            if (e.target.innerText == correctAnswer){
+                console.log("correct");
+                e.target.classList.toggle('correct');
+                setTimeout(()=> {
+                    e.target.classList.toggle('correct');
+                }, 1000)
+
+            } else {
+                e.target.classList.toggle('wrong');
+                console.log("wrong");
+                setTimeout(()=> {
+                    e.target.classList.toggle('wrong');
+                }, 1000)
+            }
             iterator++;
-            renderData(quiz[iterator]);
+
+            setTimeout(()=> {
+                renderData(quiz[iterator]);
+            }, 1000)
+
         }
     });
+
+
+//             answerArea.innerText = "";
+//             h2.innerText = "";
+//             nextButton.innerHTML = `<button class="nextButton">Next question</button>`;
+//             nextButton.addEventListener('click', getData);
+//             section.appendChild(nextButton);
+//             //if (counter < 5) getData();
+//         } else {
+//             alert("wrong answer");
+//             console.log(e.target.innerText);
+//             console.log(quizQuestion[0].correct_answer);
+//             counter++;
+//             answerArea.innerText = "";
+//             h2.innerText = "";
+//             nextButton.innerHTML = `<button class="nextButton">Next question</button>`;
+//             nextButton.addEventListener('click', getData);
+//             section.appendChild(nextButton);
+//             //if (counter < 5) getData();
+//         }
+//
+//     }
+//
+// });
 
 
 
